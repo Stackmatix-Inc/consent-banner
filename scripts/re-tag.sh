7c5459c4dcac56fcca1356575b16fa0f5ad78dac
Andy C <andy@stackmatix.com>
2025-03-21 23:03:25 -0700

    test: update banner logic

--- scripts/re-tag.sh
+++ scripts/re-tag.sh
@@ -1,3 +1,8 @@
+# Get the latest tag version and increment
+LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0-test")
+CURRENT_VERSION=$(echo $LATEST_TAG | grep -o '[0-9]\+' | tail -1)
+NEW_VERSION=$((CURRENT_VERSION + 1))
+
 # Make your new changes
 npm run build
 git add .
@@ -6,6 +11,6 @@ git commit -m "test: update banner logic"
 # Push changes to branch
 git push origin test-banner
 
-# Re-tag the latest commit
-git tag -f v0.0.3-test
-git push origin -f v0.0.3-test
+# Re-tag the latest commit with incremented version
+git tag -f v0.0.$NEW_VERSION-test
+git push origin -f v0.0.$NEW_VERSION-test
